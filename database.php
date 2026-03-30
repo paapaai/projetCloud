1a07b5d0ae43fd912bcd70e57251c7917fe9258d
paapaai <165218062+paapaai@users.noreply.github.com>
2026-03-30 21:39:14 +0200

    test17

--- database.php
+++ database.php
@@ -1,24 +1,26 @@
 <?php
+
 function getDatabaseConnection() {
-    
-$host = getenv("DB_HOST") ?: "projet-cloud.mysql.database.azure.com";
-$dbname = getenv("DB_NAME") ?: "projet_cloud_db";
-$username = getenv("DB_USER") ?: "adminazure";
-$password = getenv("DB_PASS") ?: "Azerty123!";
 
-try {
-    $pdo = new PDO(
-        "mysql:host=$host;port=3306;dbname=$dbname;charset=utf8",
-        $username,
-        $password,
-        [
-            PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/ca-certificates.crt',
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-        ]
-    );
+    $host = getenv("DB_HOST") ?: "projet-cloud.mysql.database.azure.com";
+    $dbname = getenv("DB_NAME") ?: "projet_cloud_db";
+    $username = getenv("DB_USER") ?: "adminazure@projet-cloud";
+    $password = getenv("DB_PASS") ?: "TON_MDP";
 
-} catch (PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
+    try {
+        $pdo = new PDO(
+            "mysql:host=$host;port=3306;dbname=$dbname;charset=utf8",
+            $username,
+            $password,
+            [
+                PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/ca-certificates.crt',
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ]
+        );
+
+        return $pdo;
+
+    } catch (PDOException $e) {
+        die("Erreur DB : " . $e->getMessage());
+    }
 }
-}
-?>
