5773a614edc39d1eea5c860960efaf2dcaecee88
paapaai <165218062+paapaai@users.noreply.github.com>
2026-03-30 21:03:21 +0200

    test

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-echo "HELLO AZURE 🔥";
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+require_once "config.php";
+require_once "database.php";
+
+$db = getDatabaseConnection();
+
+$page = $_GET['page'] ?? 'home';
+$pageFile = "pages";
 ?>
