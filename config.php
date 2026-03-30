c9ca84659e334e39dfadb24d35510d4aa212b401
paapaai <165218062+paapaai@users.noreply.github.com>
2026-03-30 20:30:54 +0200

    Update DB_USER definition in config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // En local (modifie si besoin)
 define("DB_HOST", getenv("DB_HOST") ?: "projet-cloud.mysql.database.azure.com");
-define("DB_USER", getenv("DB_USER") ?: "adminazure");
+define("DB_USER", getenv("DB_USER") ?: "adminazure@projet-cloud");
 define("DB_PASS", getenv("DB_PASS") ?: "Azerty123!");
 define("DB_NAME", getenv("DB_NAME") ?: "projet_cloud_db");
 ?>
