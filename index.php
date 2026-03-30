763fd44aa3de5b6e85b0329086606be3cfcdf631
paapaai <165218062+paapaai@users.noreply.github.com>
2026-03-30 21:27:31 +0200

    test12

--- index.php
+++ index.php
@@ -2,27 +2,11 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-echo "INDEX OK<br>";
-
 require_once "config.php";
-echo "CONFIG OK<br>";
-
 require_once "database.php";
-echo "DB FILE OK<br>";
 
 $db = getDatabaseConnection();
-echo "DB CONNECTED<br>";
 
-$page = $_GET['page'] ?? 'home';
-echo "PAGE = " . $page . "<br>";
-
-$file = __DIR__ . "/" . $page . ".php";
-echo "FILE = " . $file . "<br>";
-
-if (file_exists($file)) {
-    echo "FILE EXISTS<br>";
-    require $file;
-} else {
-    echo "Page introuvable";
-}
+$page = $_GET['page'];
+$pageFile = "pages";
+?>
