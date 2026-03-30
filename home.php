9a53df72bf1f2e016ebe3137aa81cd191749ef0d
paapaai <165218062+paapaai@users.noreply.github.com>
2026-03-30 21:28:48 +0200

    Delete home.php

--- home.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "database.php";
-$stmt = $db->query("SELECT 'Hello from Azure!' as message");
-$row = $stmt->fetch();
-?>
-
-<h1>Bienvenue sur mon back PHP</h1>
-<p>Message de la base de données : <strong><?= $row['message'] ?></strong></p>
